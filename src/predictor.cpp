1821059d20183bba2a669ccb7540d47ef9605c40
Mayank Kumar <mayank.kumar1123@gmail.com>
2025-02-09 15:21:10 -0800

    Update tourney values

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -128,6 +128,7 @@ void cleanup_gshare()
 
 // t = table
 // r = register
+
 #define tourney_lhistoryBits 10    // Number of bits used for Local Pattern History
 #define tourney_lbhthistoryBits 16 // Number of bits used for Local BHT
 #define tourney_ghistoryBits 16    // Number of bits used for Global History
@@ -135,73 +136,63 @@ void cleanup_gshare()
 
 uint64_t tourney_global_hr;
 
-uint16_t tourney_local_ht[1 << tourney_lhistoryBits];
-uint8_t tourney_local_pred[1 << tourney_lhistoryBits];
-uint8_t tourney_global_pred[1 << tourney_ghistoryBits];
-uint8_t tourney_choice_pred[1 << tourney_choiceBits];
+uint16_t *tourney_local_ht;
+uint8_t *tourney_local_pred;
+uint8_t *tourney_global_pred;
+uint8_t *tourney_choice_pred;
 
 void init_tourney()
 {
-    int local_bht_entries = 1 << tourney_lhistoryBits;
-    int i = 0;
-    for (i = 0; i < local_bht_entries; i++)
-    {
-        tourney_local_ht[i] = 0;
-    }
+  int local_bht_entries = 1 << tourney_lhistoryBits;
+  tourney_local_ht = (uint16_t *) malloc (sizeof(uint16_t) * local_bht_entries);
+  tourney_local_pred = (uint8_t *) malloc (sizeof(uint8_t) * local_bht_entries);
 
-    int local_pbht_entries = 1 << tourney_lbhthistoryBits;
-    for (i = 0; i < local_pbht_entries; i++)
-    {
-        tourney_local_pred[i] = WN;
-    }
+  for (int i = 0; i < local_bht_entries; i++)
+  {
+    tourney_local_ht[i] = 0;
+  }
 
-    int global_bht_entries = 1 << tourney_ghistoryBits;
-    for (i = 0; i < global_bht_entries; i++)
-    {
-        tourney_global_pred[i] = WN;
-    }
+  for (int i = 0; i < local_bht_entries; i++)
+  {
+    tourney_local_pred[i] = WN;
+  }
 
-    int choice_t_entries = 1 << tourney_choiceBits;
-    for (i = 0; i < choice_t_entries; i++)
-    {
-        tourney_choice_pred[i] = WT;
-    }
-    tourney_global_hr = 0;
+  int global_bht_entries = 1 << tourney_ghistoryBits;
+  tourney_global_pred = (uint8_t *) malloc (sizeof(uint8_t) * global_bht_entries);
+  for (int i = 0; i < global_bht_entries; i++)
+  {
+    tourney_global_pred[i] = WN;
+  }
+
+  int choice_t_entries = 1 << tourney_choiceBits;
+  tourney_choice_pred = (uint8_t *) malloc (sizeof(uint8_t) * choice_t_entries);
+  for (int i = 0; i < choice_t_entries; i++)
+  {
+    tourney_choice_pred[i] = WN;
+  }
+
+  tourney_global_hr = 0;
 }
+
 
 
 uint8_t tourney_predict_global(uint32_t pc)
 {
-    uint32_t global_bht_entries = 1 << tourney_ghistoryBits;
-    uint32_t index = tourney_global_hr & (global_bht_entries - 1);
-
-    return (tourney_global_pred[index] >= WT) ? TAKEN : NOTTAKEN;
+    uint16_t global_index = tourney_global_hr & ((1 << tourney_ghistoryBits) - 1);
+    return (tourney_global_pred[global_index] >= WT) ? TAKEN : NOTTAKEN;
 }
 
 uint8_t tourney_predict_local(uint32_t pc)
 {
-    uint32_t local_bht_entries = 1 << tourney_lhistoryBits;
-    uint32_t local_pbht_entries = 1 << tourney_lbhthistoryBits;
-    uint16_t pht_index = pc & (local_bht_entries - 1);
-    uint16_t index = tourney_local_ht[pht_index];
-
-    if (index > 1023) index = 0;
-
-    return (tourney_local_pred[index] >= WT) ? TAKEN : NOTTAKEN;
+    uint16_t pht_index = pc & ((1 << tourney_lhistoryBits) - 1);
+    uint16_t local_index = tourney_local_ht[pht_index];
+    return (tourney_local_pred[local_index] >= WT) ? TAKEN : NOTTAKEN;
 }
 
 uint8_t tourney_predict(uint32_t pc)
 {
-    uint8_t local_pred = tourney_predict_local(pc);
-    uint8_t global_pred = tourney_predict_global(pc);
-
-    uint32_t choice_entries = 1 << tourney_choiceBits;
-    uint32_t index = (tourney_global_hr ^ pc) & (choice_entries - 1);
-
-    if (tourney_choice_pred[index] >= WT)
-        return global_pred;
-    else
-        return local_pred;
+    uint16_t choice_index = tourney_global_hr & ((1 << tourney_ghistoryBits) - 1);
+    return tourney_choice_pred[choice_index] >= WT ? tourney_predict_global(pc) : tourney_predict_local(pc);
 }
 
 void train_tourney(uint32_t pc, uint8_t outcome)
@@ -228,7 +219,6 @@ void train_tourney(uint32_t pc, uint8_t outcome)
     uint32_t local_pbht_entries = 1 << tourney_lbhthistoryBits;
     uint32_t pht_index = pc & (local_bht_entries - 1);
     uint32_t local_index = tourney_local_ht[pht_index];
-    if (local_index > 1023) local_index = 0;
 
     if (outcome == TAKEN)
     {
